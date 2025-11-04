5f5365b478f6c0ffa79550df20cd42d5fefc6cc7
urosbogi <urosbog91@gmail.com>
2025-11-04 22:08:32 +0200

    update project

---
+++ ProjectTwoNew/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ProjectTwoNew;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input your paragraph.");
+        string paragraph = Console.ReadLine();
+        string[] words = paragraph.Split();
+        int wordCount = 0;
+        Console.WriteLine($"The word count is {wordCount = words.Length} ");
+
+        int letterNumber = 0;
+        foreach (string word in words)
+        {
+            int templetterNumber = word.Length;
+            letterNumber = templetterNumber + letterNumber;
+        }
+        Console.WriteLine($"test number: {letterNumber}");
+
+        // Console.WriteLine("testing to see the results: {1}", paragraph, paragraph.Length);
+        
+    }
+}
